c2bf3d586c5ecb237eb69a86f3632273c509ce2d
Edwin Gustafson <edwingustafson@gmail.com>
2020-04-21 16:11:06 -0400

    Format and lint

--- script.js
+++ script.js
@@ -5,29 +5,29 @@ Object.assign(window, {
 	dimRate: 0.1,	//	depends on colorMode range parameter
 
 	setup: () => {
-	  colorMode(HSB, 1.0);
-	  frameRate(30);
+		colorMode(HSB, 1.0);
+		frameRate(30);
 
-	  createCanvas(windowWidth, windowHeight);
+		createCanvas(windowWidth, windowHeight);
 		ellipseMode(CENTER);
 
-	  $.reset();
+		$.reset();
 	},
 
 	draw: () => {
 		$.transform();
-	  $.dim();
+		$.dim();
 
 		stroke(t, 1.0, 1.0);
-		const radius = 2.0 * Math.sin( 2.0 * Math.PI * t );
+		const radius = 2.0 * Math.sin(2.0 * Math.PI * t);
 		ellipse(0.0, 0.0, radius, radius);
 
 		t = (t + dt) % 1.0;
 	},
 
 	windowResized: () => {
-	  resizeCanvas(windowWidth, windowHeight);
-	  $.reset();
+		resizeCanvas(windowWidth, windowHeight);
+		$.reset();
 	},
 
 	$: {
@@ -35,14 +35,12 @@ Object.assign(window, {
 			background(0, dimRate);
 		},
 
-		random: (limit) => {
-			return Math.random() * limit;
-		},
+		random: (limit) => Math.random() * limit,
 
 		reset: () => {
-			translate(windowWidth/2, windowHeight/2);
+			translate(windowWidth / 2, windowHeight / 2);
 			rotate(radians(-90));
-			scale(0.9 * Math.min(windowWidth/2, windowHeight/2));
+			scale(0.9 * Math.min(windowWidth / 2, windowHeight / 2));
 
 			background(0);
 
@@ -53,9 +51,9 @@ Object.assign(window, {
 		},
 
 		transform: () => {
-			translate(windowWidth/2, windowHeight/2);
+			translate(windowWidth / 2, windowHeight / 2);
 			rotate(radians(-90));
-			scale(0.9 * Math.min(windowWidth/2, windowHeight/2));
+			scale(0.9 * Math.min(windowWidth / 2, windowHeight / 2));
 		}
 	}
 });
