0c6cca0434219026e0076350139aeb92e0ae6344
Edwin Gustafson <edwingustafson@gmail.com>
2020-05-11 15:18:58 -0400

    deno fmt

--- script.js
+++ script.js
@@ -1,61 +1,61 @@
 Object.assign(window, {
-	t: 0.0,
-	dt: 0.005,
+  t: 0.0,
+  dt: 0.005,
 
-	dimRate: 0.1,	//	depends on colorMode range parameter
+  dimRate: 0.1, //	depends on colorMode range parameter
 
-	setup: () => {
-	  colorMode(HSB, 1.0);
-	  frameRate(30);
+  setup: () => {
+    colorMode(HSB, 1.0);
+    frameRate(30);
 
-	  createCanvas(windowWidth, windowHeight);
-		ellipseMode(CENTER);
+    createCanvas(windowWidth, windowHeight);
+    ellipseMode(CENTER);
 
-	  $.reset();
-	},
+    $.reset();
+  },
 
-	draw: () => {
-		$.transform();
-	  $.dim();
+  draw: () => {
+    $.transform();
+    $.dim();
 
-		stroke(t, 1.0, 1.0);
-		const radius = 2.0 * Math.sin( 2.0 * Math.PI * t );
-		ellipse(0.0, 0.0, radius, radius);
+    stroke(t, 1.0, 1.0);
+    const radius = 2.0 * Math.sin(2.0 * Math.PI * t);
+    ellipse(0.0, 0.0, radius, radius);
 
-		t = (t + dt) % 1.0;
-	},
+    t = (t + dt) % 1.0;
+  },
 
-	windowResized: () => {
-	  resizeCanvas(windowWidth, windowHeight);
-	  $.reset();
-	},
+  windowResized: () => {
+    resizeCanvas(windowWidth, windowHeight);
+    $.reset();
+  },
 
-	$: {
-		dim: () => {
-			background(0, dimRate);
-		},
+  $: {
+    dim: () => {
+      background(0, dimRate);
+    },
 
-		random: (limit) => {
-			return Math.random() * limit;
-		},
+    random: (limit) => {
+      return Math.random() * limit;
+    },
 
-		reset: () => {
-			translate(windowWidth/2, windowHeight/2);
-			rotate(radians(-90));
-			scale(0.9 * Math.min(windowWidth/2, windowHeight/2));
+    reset: () => {
+      translate(windowWidth / 2, windowHeight / 2);
+      rotate(radians(-90));
+      scale(0.9 * Math.min(windowWidth / 2, windowHeight / 2));
 
-			background(0);
+      background(0);
 
-			noFill();
-			strokeWeight(0.0025);
+      noFill();
+      strokeWeight(0.0025);
 
-			t = 0.0;
-		},
+      t = 0.0;
+    },
 
-		transform: () => {
-			translate(windowWidth/2, windowHeight/2);
-			rotate(radians(-90));
-			scale(0.9 * Math.min(windowWidth/2, windowHeight/2));
-		}
-	}
+    transform: () => {
+      translate(windowWidth / 2, windowHeight / 2);
+      rotate(radians(-90));
+      scale(0.9 * Math.min(windowWidth / 2, windowHeight / 2));
+    },
+  },
 });
